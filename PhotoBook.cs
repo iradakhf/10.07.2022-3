48cc81d53541bead9d4f37c6384db147a6dae4c7
iradakhf <iradakhf@code.edu.az>
2022-07-10 14:42:58 +0400

    add all

---
+++ PhotoBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    public class PhotoBook
+    {
+        protected int numPages;
+        public  int GetNumberPages()
+        {
+            return numPages;
+        }
+        public PhotoBook()
+        {
+            numPages = 16;
+        }
+        public PhotoBook(int numPages)
+        {
+            this.numPages = numPages;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp23
+{
+   public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            //PhotoBook photobook = new ();
+            //Console.WriteLine(photobook.GetNumberPages());
+
+            //PhotoBook photoB = new(15);
+            //Console.WriteLine(photoB.GetNumberPages());
+
+            BigPhotoBook bigPhotoBookPages = new();
+            Console.WriteLine(bigPhotoBookPages.GetNumberPages()); 
+            
+        }
+    }
+}
